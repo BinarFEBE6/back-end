acb864a16e69bd2df7f267975966f81550c44bfd
ingwerludwig <ingwerflash@gmail.com>
2022-12-26 04:00:19 +0700

    chore: testing unit testing functionality

--- src/test/java/org/binaracademy/finalproject/services/UserServiceTest.java
+++ src/test/java/org/binaracademy/finalproject/services/UserServiceTest.java
@@ -7,12 +7,14 @@ import org.binaracademy.finalproject.services.impl.UserServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Locale;
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,6 +22,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
     @Mock
