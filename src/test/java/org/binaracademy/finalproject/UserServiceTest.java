077f1c1043c8894c834e645f569c07597485a2a4
ingwerludwig <ingwerflash@gmail.com>
2022-12-26 03:23:35 +0700

    chore: testing unit testing functionality

--- src/test/java/org/binaracademy/finalproject/UserServiceTest.java
+++ src/test/java/org/binaracademy/finalproject/UserServiceTest.java
@@ -1,4 +1,4 @@
-package org.binaracademy.finalproject.services;
+package org.binaracademy.finalproject;
 
 import com.github.javafaker.Faker;
 import org.binaracademy.finalproject.entity.UserEntity;
