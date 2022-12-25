d415c2dc4e4b01dfeb1413a77612c814948f7aa8
ingwerludwig <ingwerflash@gmail.com>
2022-12-26 03:45:59 +0700

    chore: testing unit testing functionality

--- src/test/java/org/binaracademy/finalproject/services/UserServiceTest.java
+++ src/test/java/org/binaracademy/finalproject/services/UserServiceTest.java
@@ -7,7 +7,6 @@ import org.binaracademy.finalproject.services.impl.UserServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
