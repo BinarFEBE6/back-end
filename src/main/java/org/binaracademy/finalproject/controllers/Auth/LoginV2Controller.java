95d651ec4c06b3a5a2a9941dbb8c20c4210a063b
ingwerludwig <ingwerflash@gmail.com>
2022-12-29 00:05:50 +0700

    chore: change controller name

--- src/main/java/org/binaracademy/finalproject/controllers/Auth/LoginV2Controller.java
+++ src/main/java/org/binaracademy/finalproject/controllers/Auth/LoginV2Controller.java
@@ -37,9 +37,9 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/api/auth")
 @Tag(name = "Auth", description = "Operation signin and signup")
-public class Oauth2Controller {
+public class LoginV2Controller {
 
-    public static final Logger logger = LoggerFactory.getLogger(Oauth2Controller.class);
+    public static final Logger logger = LoggerFactory.getLogger(LoginV2Controller.class);
     public static final String ROLE_NOT_FOUND = "Error: Role is not found.";
     @Autowired
     AuthenticationManager authenticationManager;
@@ -58,38 +58,6 @@ public class Oauth2Controller {
     @Autowired
     JwtUtils jwtUtils;
 
-//    @Operation(summary = "Login (EndPoint untuk user Login with google)")
-//    @ApiResponses(value = {
-//            @ApiResponse(responseCode = "200", description = "sukses", content = @Content(examples = {
-//                    @ExampleObject(name = "User Login with google",
-//                            description = "EndPoint ini digunakan untuk user dapat melakukan login with google",
-//                            value = "{\n"
-//                                    + "    \"success\": true,\n"
-//                                    + "    \"statusCode\": 200,\n"
-//                                    + "    \"message\": \"sukses\",\n"
-//                                    + "    \"data\":{\n"
-//                                    + "            \"token\": \"gd732r63839rg773.9fggf783g387gd.9qwgf87qgr37\",\n"
-//                                    + "            \"type\": \"Bearer\",\n"
-//                                    + "            \"id\": 1,\n"
-//                                    + "            \"username\": \"budi123\",\n"
-//                                    + "            \"email\": \"budi@gmail.com\",\n"
-//                                    + "            \"roles\": [\n"
-//                                    + "                \"ROLE_USER\"\n"
-//                                    + "              ]\n"
-//                                    + "        }\n"
-//                                    + "}")
-//            }, mediaType = MediaType.APPLICATION_JSON_VALUE)),
-//            @ApiResponse(responseCode = "500", content = @Content(examples = {
-//                    @ExampleObject(name = "Server Error",
-//                            description = "Tampilan jika server error",
-//                            value = "{\n"
-//                                    + "    \"success\": false,\n"
-//                                    + "    \"statusCode\": 500,\n"
-//                                    + "    \"message\": \"Server Error Message\",\n"
-//                                    + "    \"data\": null\n"
-//                                    + "}")
-//            }, mediaType = MediaType.APPLICATION_JSON_VALUE))
-//    })
     @PostMapping("/getRedirect")
     public ResponseEntity<ResponseData<JwtResponse>> authenticateUser(@Valid @RequestBody LoginV2Request loginV2Request, Errors errors) {
 
