97c7b3b257dcb4996ef2b013a36006a19783ff54
ingwerludwig <ingwerflash@gmail.com>
2022-12-30 23:36:56 +0700

    chore: change unit testing annotation

--- src/test/java/org/binaracademy/finalproject/services/UsersDetailsServiceTest.java
+++ src/test/java/org/binaracademy/finalproject/services/UsersDetailsServiceTest.java
@@ -45,54 +45,52 @@ class UsersDetailsServiceTest {
     @Mock
     UsersDetailsRepo repository;
 
-    @Rule //initMocks
-    public MockitoRule rule = MockitoJUnit.rule();
-
     @InjectMocks
     UsersDetailsServiceImpl userDetailsServiceImpl = new UsersDetailsServiceImpl();
     Faker faker;
 
     @Before
     public void setUp() {
+        MockitoAnnotations.initMocks(this); //without this you will get NPE
         faker = new Faker(new Locale("en-US"));
     }
 
-    @DisplayName("Create User Details Test")
-    @Test
-    void create() {
-        assertNotNull(userDetailsServiceImpl);
-        faker = new Faker(new Locale("en-US"));
-
-        UserDetailsEntity newUser = new UserDetailsEntity(
-                LocalDate.of(2022, 9, 1),
-                "Mr. ",
-                faker.address().streetAddress(),
-                faker.name().name()
-        );
-
-        when(repository.save(any(UserDetailsEntity.class)))
-                .thenReturn(newUser);
-
-        assertEquals(userDetailsServiceImpl.create(newUser),newUser);
-        Mockito.verify(repository, Mockito.times(1)).save(newUser);
-    }
-
-
+//    @DisplayName("Create User Details Test")
 //    @Test
-//    @DisplayName("Get User Details Test")
-//    void findByUserid() {
+//    void create() {
 //        assertNotNull(userDetailsServiceImpl);
 //        faker = new Faker(new Locale("en-US"));
 //
-//        Set<RoleEntity> s = new HashSet<RoleEntity>();
-//        UserEntity newUserEntity = new UserEntity(2L,faker.name().name(),faker.internet().emailAddress(),faker.internet().password(),"TEST",LocalDateTime.now(),LocalDateTime.now(),s);
-//        UserDetailsEntity newUser = new UserDetailsEntity(1L,faker.name().name(),LocalDate.of(2022, 9, 1),"Mr.",faker.address().fullAddress(),2L,newUserEntity, LocalDateTime.now(),LocalDateTime.now());
+//        UserDetailsEntity newUser = new UserDetailsEntity(
+//                LocalDate.of(2022, 9, 1),
+//                "Mr. ",
+//                faker.address().streetAddress(),
+//                faker.name().name()
+//        );
 //
-//        when(repository.findUserDetailsByUserId(2L))
-//                .thenReturn(Optional.of(newUser));
+//        when(repository.save(any(UserDetailsEntity.class)))
+//                .thenReturn(newUser);
 //
-//        assertEquals(userDetailsServiceImpl.findByUserid(2L),newUser);
-//        Mockito.verify(repository, Mockito.times(1)).findUserDetailsByUserId(2L);
+//        assertEquals(userDetailsServiceImpl.create(newUser),newUser);
+//        Mockito.verify(repository, Mockito.times(1)).save(newUser);
 //    }
+
+
+    @Test
+    @DisplayName("Get User Details Test")
+    void findByUserid() {
+        assertNotNull(userDetailsServiceImpl);
+        faker = new Faker(new Locale("en-US"));
+
+        Set<RoleEntity> s = new HashSet<RoleEntity>();
+        UserEntity newUserEntity = new UserEntity(2L,faker.name().name(),faker.internet().emailAddress(),faker.internet().password(),"TEST",LocalDateTime.now(),LocalDateTime.now(),s);
+        UserDetailsEntity newUser = new UserDetailsEntity(1L,faker.name().name(),LocalDate.of(2022, 9, 1),"Mr.",faker.address().fullAddress(),2L,newUserEntity, LocalDateTime.now(),LocalDateTime.now());
+
+        when(repository.findUserDetailsByUserId(2L))
+                .thenReturn(Optional.of(newUser));
+
+        assertEquals(userDetailsServiceImpl.findByUserid(2L),newUser);
+        Mockito.verify(repository, Mockito.times(1)).findUserDetailsByUserId(2L);
+    }
 
 }
